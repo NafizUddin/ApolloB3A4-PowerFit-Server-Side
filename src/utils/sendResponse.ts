224bfdcd0a9ecf53b60080b897c1195e24590e08
NafizUddin <nafizuddin.ctg@gmail.com>
2024-07-09 05:00:43 +0600

    added catchAsync and sendResponse function

---
+++ src/utils/sendResponse.ts
@@ -0,0 +1,13 @@
+import { Response } from 'express';
+import { TResponse } from '../interface/sendResponseInterface';
+
+const sendResponse = <T>(res: Response, data: TResponse<T>) => {
+  return res.status(data?.statusCode).json({
+    success: data?.success,
+    statusCode: data?.statusCode,
+    message: data?.message,
+    data: data?.data,
+  });
+};
+
+export default sendResponse;
